c58076c442bfe2927eb37bef8909188bb997b469
Vishalthakur154837 <vt883854@gmail.com>
2026-04-29 10:43:38 +0530

    Add files via upload

---
+++ Library/src/Main.java
@@ -0,0 +1,18 @@
+//package Library;
+
+public class Main {
+    public static void main(String[] args) {
+        Library lib = new Library();
+
+        lib.addBook(new Book(1, "Java Basics"));
+        lib.addBook(new Book(2, "Data Structures"));
+
+        lib.addMember(new Member(101, "Utkrisht"));
+
+        lib.issueBook(1, 101);
+        lib.showBooks();
+
+        lib.returnBook(1, 101);
+        lib.showBooks();
+    }
+}
